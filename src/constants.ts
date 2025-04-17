cb86fc39654518045d44204be6469bb7602bf3cc
RAJASHREE-BANDI <bandirs2003@gmail.com>
2025-04-17 23:06:13 +0530

    chore : resolved conflicts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const BASE_URL = '/api/v1';
-export const RESET_TOKEN_EXPIRY_TIME=Date.now() + 15 * 60 * 1000;
+export const RESET_TOKEN_EXPIRY_TIME=Date.now() + 15 * 60 * 1000
+export const LOG_LEVEL = 'info';
+export const LOG_FORMAT = 'YYYY-MM-DD HH:mm:ss.SSSZZ';
