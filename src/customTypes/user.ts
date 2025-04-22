9d8238e8eb7ebc9a715560ce544e4e88d660c872
Manoo07 <manoharboinapalli1196@gmail.com>
2025-04-22 09:16:22 +0530

    (feat) : College CRUD

---
+++ src/customTypes/user.ts
@@ -0,0 +1,14 @@
+import { Role } from '@prisma/client';
+
+export interface SignupParams {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+  collegeId: string;
+  role: Role;
+  departmentId: string;
+  sectionId: string;
+  specialization?: string;
+}
